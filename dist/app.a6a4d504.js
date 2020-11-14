54c551d283f4ca9d1160b064cfaa2a3705d5b48a
hef <hefan813@vip.qq.com>
2020-11-14 20:01:11 +0800

    '添加icon和iconPosition属性'

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12438,7 +12438,27 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  // props: ["icon", "iconPosition"],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(val) {
+        return !(val !== 'left' && val !== 'right');
+      }
+    }
+  }
+};
 exports.default = _default;
         var $105a00 = exports.default || module.exports;
       
@@ -12449,10 +12469,26 @@ exports.default = _default;
         /* template */
         Object.assign($105a00, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#icon-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12496,6 +12532,8 @@ var _button = _interopRequireDefault(require("./button.vue"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+console.log(_button.default);
+
 _vue.default.component('g-button', _button.default);
 
 new _vue.default({
@@ -12529,7 +12567,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59348" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61644" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,20 +1,43 @@
 <template>
-<button class="g-button">按钮</button>
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+    
+  </button>
 </template>
 
 <script>
 export default {
-}
+  // props: ["icon", "iconPosition"],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(val){
+        return !(val !== 'left' && val !== 'right')
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 1em;
+  padding: 0 .9em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -23,6 +46,23 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  > .icon{
+    order: 1;
+    margin-right: 0.3em;
+  }
+  > .content{
+    order: 2;
+  }
+  &.icon-right {
+    > .content{
+      order: 1
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em;
+    }
   }
 }
 </style>
