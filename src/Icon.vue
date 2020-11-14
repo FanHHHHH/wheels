410eb15b519fe56701e3ab88d5ffccfe4b47ad1d
hef <hefan813@vip.qq.com>
2020-11-14 20:20:59 +0800

    将svg 代码整理到 icon.vue

---
+++ src/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg class="icon">
+      <use :xlink:href="`#icon-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {}
+    }
+}
+</script>
+
+<style>
+
+</style>
