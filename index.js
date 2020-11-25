dd0923aa4ade3f62d1de814222e9088f734e66ef
hef <hefan813@vip.qq.com>
2020-11-25 16:31:03 +0800

    修改index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import Button from './src/button'
-import ButtonGroup from './src/button-group'
-import Icon from './src/Icon'
-import Input from './src/Input'
-// export {ButtonGroup, Button, Icon, Input}
