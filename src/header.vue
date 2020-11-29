199244d683224bbc0eae5ae6c753e64c689d645a
hef <hefan813@vip.qq.com>
2020-11-29 21:38:38 +0800

    添加简单动画（js动态获取sider宽度未实现，需要volocity）

--- src/header.vue
+++ src/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div class="header">
       <slot></slot>
   </div>
 </template>
@@ -9,7 +9,4 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.content {
-  flex-grow: 1;
-}
 </style>
---
+++ src/sider.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition name="slide">
+    <div class="sider" v-show="visable">
+      <slot></slot>
+      <button class="closeButton" @click="hide">close</button>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "GuluSider",
+  data() {
+    return {
+      visable: true,
+      show: true,
+    };
+  },
+  methods: {
+    hide() {
+      this.visable = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter,
+.slide-leave-to { 
+  margin-left: -200px;
+}
+</style>
