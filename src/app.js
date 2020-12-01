d0b483af2403f96c7a88d2dead16c667e718c1bd
hef <hefan813@vip.qq.com>
2020-12-01 22:19:12 +0800

    完成toast测试用例

--- src/app.js
+++ src/app.js
@@ -58,10 +58,7 @@ new Vue({
         showToast(position) {
             this.$toast('当前功能不稳定，如果遇到bug，请关闭该功能' + parseInt(Math.random() * 100), {
                 text: '知道了',
-                callback(toast) {
-                    toast.log()
-                    console.log('用户知道了');
-                },
+                autoClose: false,
                 position
             })
         },
--- src/toast.vue
+++ src/toast.vue
@@ -18,12 +18,11 @@ export default {
   name: "GToast",
   props: {
     autoClose: {
-      type: Boolean,
-      default: true,
-    },
-    autoCloseDelay: {
-      type: Number,
-      default: 50,
+      type: [Boolean, Number],
+      default: 5,
+      validator(val) {
+        return val === false || typeof val === 'number'
+      }
     },
     closeButton: {
       type: Object,
@@ -62,7 +61,7 @@ export default {
       if (this.autoClose) {
         setTimeout(() => {
           this.close();
-        }, this.autoCloseDelay * 1000);
+        }, this.autoClose * 1000);
       }
     },
     close() {
