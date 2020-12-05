8e837bfbfb9b7320e7410616e2134f7f86cba165
hef <hefan813@vip.qq.com>
2020-12-05 22:14:45 +0800

    解决当容器有overflow:hidden bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="onClick">
-    <div v-if="visable" class="content-wrapper" @click.stop>
+    <div v-if="visable" ref="contentWrapper" class="content-wrapper">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <div ref="triggerWrapper">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -18,35 +20,36 @@ export default {
   methods: {
     onClick() {
       this.visable = !this.visable;
+
       let eventHandler = () => {
-        console.log("document隐藏");
         this.visable = false;
         document.removeEventListener("click", eventHandler);
       };
       if (this.visable) {
-        setTimeout(() => {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let { width, height, top, left, } = this.$refs.triggerWrapper.getBoundingClientRect();
+          this.$refs.contentWrapper.style.left = left + window.screenX + "px";
+          this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
           document.addEventListener("click", eventHandler);
         });
-      } else {
-          console.log("vm隐藏");
       }
     },
   },
+  mounted() {},
 };
 </script>
 
 <style lang='scss' scoped>
 .popover {
-  border: 1px solid salmon;
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid aquamarine;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid aquamarine;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
 }
 </style>
