ec216197fa8d11ccaa6f3dceb9038e6c9be0343e
hef <hefan813@vip.qq.com>
2020-12-11 14:27:12 +0800

    一种错误的cascader实现

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
+    <div class="trigger" @click="popoverVisable = !popoverVisable">
       <slot></slot>
-      <div v-for="item in source" :key="item.name">
+      <!-- <div v-for="item in source" :key="item.name">
         <cascader-item :sourceItem="item"></cascader-item>
+      </div> -->
+    </div>
+    <div class="popover" v-if="popoverVisable">
+      <div class="level1">
+        <div class="item" v-for="item1 in source" :key="item1.name" @click="level1Selected = item1.children">{{ item1.name }}</div>
+      </div>
+      <div class="level2">
+        <div class="item" v-for="item2 in level2Item" :key="item2.name" @click="level2Selected = item2.children">{{ item2.name }}</div>
+      </div>
+      <div class="level3">
+        <div class="item" v-for="item3 in level3Item" :key="item3.name">{{ item3.name }}</div>
       </div>
     </div>
   </div>
@@ -14,18 +25,33 @@ import CascaderItem from "./cascader-item.vue";
 export default {
   name: "GuluCascader",
   components: {
-    CascaderItem
+    CascaderItem,
   },
   props: {
     source: {
       type: Array,
     },
   },
-  methods: {
-    // getDatas(arr) {
-    //   if (typeof arr !== "array") {
-    //   }
-    // },
+  data() {
+    return {
+      popoverVisable: false,
+      level1Selected: null,
+      level2Selected: null,
+    };
+  },
+  computed: {
+    level2Item() {
+      if (this.level1Selected) {
+        return this.level1Selected;
+      }
+      return null;
+    },
+    level3Item() {
+      if (this.level2Selected) {
+        return this.level2Selected;
+      }
+      return null;
+    },
   },
 };
 </script>
@@ -33,5 +59,31 @@ export default {
 <style lang="scss" scoped>
 @import "var";
 .cascader {
+  > .trigger {
+    border: 1px solid salmon;
+    width: 80px;
+    height: 30px;
+  }
+  > .popover {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid salmon;
+    
+    > .level1 {
+      border: 1px solid forestgreen;
+      width: 100px;
+      height: 100px;
+    }
+    > .level2 {
+      border: 1px solid salmon;
+      width: 100px;
+      height: 100px;
+    }
+    > .level3 {
+      border: 1px solid slategrey;
+      width: 100px;
+      height: 100px;
+    }
+  }
 }
 </style>
