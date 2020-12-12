0fb0004bc07432430a3d34927e6990ba49df1df2
hef <hefan813@vip.qq.com>
2020-12-12 14:06:52 +0800

    确定如何渲染内容

---
+++ src/cascader-items.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cascaderItems">
+    <div class="left">
+      <div class="label" v-for="item in this.sourceItem" :key="item.name" @click="leftSelected = item">
+        {{ item.name }}
+      </div>
+    </div>
+    <div class="right" v-if="rightItem">
+      <gulu-cascader-items :sourceItem="rightItem"></gulu-cascader-items>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluCascaderItems",
+  props: {
+    sourceItem: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      leftSelected: null,
+    };
+  },
+  computed: {
+    rightItem() {
+      if (this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children;
+      } else {
+        return null;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cascaderItems {
+  display: flex;
+  .left {
+    border: 1px solid salmon;
+  }
+}
+</style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="cascader">
-    <div class="trigger" @click="popoverVisable = !popoverVisable">
-      <slot></slot>
-      <!-- <div v-for="item in source" :key="item.name">
-        <cascader-item :sourceItem="item"></cascader-item>
-      </div> -->
-    </div>
-    <div class="popover" v-if="popoverVisable">
-      <div class="level1">
-        <div class="item" v-for="item1 in source" :key="item1.name" @click="level1Selected = item1.children">{{ item1.name }}</div>
-      </div>
-      <div class="level2">
-        <div class="item" v-for="item2 in level2Item" :key="item2.name" @click="level2Selected = item2.children">{{ item2.name }}</div>
-      </div>
-      <div class="level3">
-        <div class="item" v-for="item3 in level3Item" :key="item3.name">{{ item3.name }}</div>
-      </div>
-    </div>
+    <div class="trigger" @click="popoverVisable = !popoverVisable"></div>
+    <cascader-items v-if="popoverVisable" :sourceItem="source"></cascader-items>
   </div>
 </template>
 
 <script>
-import CascaderItem from "./cascader-item.vue";
+import CascaderItems from "./cascader-items.vue";
 export default {
   name: "GuluCascader",
   components: {
-    CascaderItem,
+    CascaderItems,
   },
   props: {
     source: {
@@ -35,23 +20,7 @@ export default {
   data() {
     return {
       popoverVisable: false,
-      level1Selected: null,
-      level2Selected: null,
     };
-  },
-  computed: {
-    level2Item() {
-      if (this.level1Selected) {
-        return this.level1Selected;
-      }
-      return null;
-    },
-    level3Item() {
-      if (this.level2Selected) {
-        return this.level2Selected;
-      }
-      return null;
-    },
   },
 };
 </script>
@@ -63,27 +32,6 @@ export default {
     border: 1px solid salmon;
     width: 80px;
     height: 30px;
-  }
-  > .popover {
-    display: flex;
-    flex-direction: row;
-    border: 1px solid salmon;
-    
-    > .level1 {
-      border: 1px solid forestgreen;
-      width: 100px;
-      height: 100px;
-    }
-    > .level2 {
-      border: 1px solid salmon;
-      width: 100px;
-      height: 100px;
-    }
-    > .level3 {
-      border: 1px solid slategrey;
-      width: 100px;
-      height: 100px;
-    }
   }
 }
 </style>
