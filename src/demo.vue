a1501bcc769b151a389e0cda183bdb1eef4406c6
hef <hefan813@vip.qq.com>
2021-01-29 15:57:55 +0800

    完成upload的图片上传

--- src/demo.vue
+++ src/demo.vue
@@ -1,96 +1,57 @@
 <template>
   <div id="app" style="margin: 100px;">
-    <b-table indexIsVisable checkable expandField="description" 
-    :index-is-visable="false" :height="300" bordered 
-    :selected-items.sync="selected" :data-source="dataSource" 
-    :columns="columns" :order-by.sync="orderBy" @update:orderBy="x" 
-    :loading="loading">
-      <template v-slot="row">
-        <button @click="edit(row.item)">编辑</button>
-        <button @click="view(row.item)">查看</button>
+    <b-uploader accept="image/*" action="http://localhost:3000/upload" name="file" method="POST">
+      <button>点击上传</button>
+      <template #tips>
+        <div>只能上传 300KB 以内的 png、jpeg 文件</div>
       </template>
-    </b-table>
-    <!-- <b-table :index-is-visable="false" :striped="false" bordered tight :data-source="dataSource" :columns="columns"></b-table> -->
-    <b-pager :total-page="10" :current-page.sync="currentPage" hide-if-one-page></b-pager>
+    </b-uploader>
+    <!-- <form id="f" ac="http://127.0.0.1:3000/upload">
+      <div>
+        <input type="file" name="file" />
+      </div>
+      <div>
+        <input type="submit" value="点击上传" />
+      </div>
+    </form> -->
+    <!-- <div>
+      <img id="img" src="" alt="" />
+    </div> -->
   </div>
 </template>
 
 <script>
-import Button from './button/button.vue'
-import Cascader from './cascader/cascader'
-import Popover from './popover'
-import Input from './Input'
-import BSlides from './slides/slides'
-import BSlidesItem from './slides/slides-item'
-import BNav from './nav/nav.vue'
-import BNavItem from './nav/nav-item.vue'
-import BSubNav from './nav/sub-nav.vue'
-import BPager from './pager.vue'
-import db from '../tests/fixtures/db.js'
-import BTable from './table.vue'
-
 import { removeListener } from './click-outside.js'
+import BUploader from './uploader.vue'
 
 export default {
   name: 'demo',
   components: {
-    'b-button': Button,
-    'b-cascader': Cascader,
-    'b-popover': Popover,
-    'b-input': Input,
-    BSlides,
-    BSlidesItem,
-    BNav,
-    BNavItem,
-    BSubNav,
-    BPager,
-    BTable,
+    BUploader,
   },
   data() {
     return {
-      currentPage: 1,
-      columns: [
-        { text: '姓名', field: 'name', width: '500' },
-        { text: '分数', field: 'score' },
-      ],
-      orderBy: {
-        name: true,
-        score: 'desc',
-      },
-      dataSource: [
-        { id: 1, name: 'hefan', score: 100, description: 'aaa bbb' },
-        { id: 2, name: 'wzx', score: 99, description: 'ccc ddd' },
-        { id: 3, name: 'aa', score: 99 },
-        { id: 4, name: 'bb', score: 99 },
-        { id: 5, name: 'cc', score: 99 },
-        { id: 6, name: 'dd', score: 99 },
-        { id: 7, name: 'ee', score: 99 },
-        { id: 8, name: 'ff', score: 99 },
-        { id: 9, name: 'gg', score: 99 },
-        { id: 10, name: 'hh', score: 99 },
-      ],
-      selected: [],
-      loading: false,
+      fileList: [],
     }
   },
-  methods: {
-    x(e) {
-      this.loading = true
-      setTimeout(() => {
-        this.dataSource.sort((a, b) => a.score - b.score)
-        this.loading = false
-      }, 2000)
-    },
-    edit(a) {
-      window.alert(a.name)
-    },
-    view(a) {
-      window.alert(a.name)
-    },
-  },
-  created() {},
-  watch: {
-    selected(newVla) {},
+  methods: {},
+  mounted() {
+    // const f = document.querySelector('#f')
+    // const img = document.querySelector('#img')
+    // f.addEventListener('submit', (e) => {
+    //   // 阻止默认事件
+    //   e.preventDefault()
+    //   var formData = new FormData(f)
+    //   // const fileInput = document.querySelector('input[name="xxx"]')
+    //   // formData
+    //   console.log(f.ac)
+    //   var xhr = new XMLHttpRequest()
+    //   xhr.open('POST', 'https://g-ui-node-server.herokuapp.com/upload')
+    //   xhr.onload = () => {
+    //     img.src = `https://g-ui-node-server.herokuapp.com/preview/${xhr.response}`
+    //   }
+    //   xhr.send(formData)
+    // })
   },
 }
 </script>
---
+++ src/uploader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="b-uploader">
+    <div @click="onClickUpload">
+      <slot></slot>
+    </div>
+    <slot name="tips"></slot>
+    <div class="clickButton" ref="clickButton"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlueUploader',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    method: {
+      type: String,
+      default: 'POST',
+    },
+  },
+  methods: {
+    onClickUpload() {
+      const input = document.createElement('input')
+      input.type = 'file'
+      this.$refs.clickButton.appendChild(input)
+      input.addEventListener('change', () => {
+        let file = input.files[0]
+        let formData = new FormData()
+        formData.append(this.name, file) // 获取文件名
+
+        const xhr = new XMLHttpRequest()
+        xhr.open(this.method, this.action)
+        xhr.onload = () => {
+          // 上传完毕
+          console.log('xhr返回', xhr.response)
+          // 反序列化
+          let obj = JSON.parse(xhr.response)
+        }
+        xhr.send(formData)
+      })
+      input.click()
+      input.remove()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.b-uploader {
+}
+</style>
